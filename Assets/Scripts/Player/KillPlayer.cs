89fa5cdd3038d2a956c9c5cc46f6026683885325
BoredBrick <67233864+BoredBrick@users.noreply.github.com>
2022-12-09 19:38:37 +0100

    add obstacles

--- Assets/Scripts/Player/KillPlayer.cs
+++ Assets/Scripts/Player/KillPlayer.cs
@@ -4,6 +4,7 @@ public class KillPlayer : MonoBehaviour
 {
     private void OnTriggerEnter(Collider obstacle)
     {
+        Debug.Log("enter");
         if (obstacle.CompareTag("Player"))
         {
             GameOver gameOverScreen = GameObject.Find("Scripts").GetComponent<GameOver>();
